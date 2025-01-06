0b7da4661592ed202df750a4fbab9bd9b85f1e20
ElianKas <135838274+ElianKas@users.noreply.github.com>
2025-01-06 14:27:55 +0100

    card

--- pages/index.vue
+++ pages/index.vue
@@ -22,12 +22,16 @@
 			<div class="h-[80px] grid place-items-center">
 				<h1>Welcome to AFACTORY the future of production.</h1>
 			</div>
-			<div class="bg-black max-sm:aspect-[3/5] w-full">
-				<div>
+			<div
+				class="bg-black max-sm:aspect-[3/5] w-full flex flex-col justify-end items-center">
+				<div class="flex flex-col items-center text-white">
 					<h2>Like Paper</h2>
 					<p>Designed by Lars Harnhardt</p>
 					<br />
-					<button class="btn"></button>
+					<button
+						class="btn bg-black text-white border-none font-normal">
+						Kollektin entdecken
+					</button>
 				</div>
 			</div>
 		</section>
