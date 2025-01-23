3952c2da082c4f51402b97b7f82a72514b7294c7
ElianKas <135838274+ElianKas@users.noreply.github.com>
2025-01-23 20:16:43 +0100

    Update Designers.vue

--- components/Designers.vue
+++ components/Designers.vue
@@ -4,24 +4,32 @@
 		<br />
 		<ul>
 			<li
-				class="border-b border-grey py-[1rem] flex gap-[1rem] items-center">
+				class="group border-b relative border-grey hover:border-black py-[1rem] flex gap-[1rem] items-center">
 				<div class="h-[70px] aspect-square border rounded-full"></div>
 				<p>Designer 1</p>
+				<div
+					class="pointer-events-none absolute hidden group-hover:block bg-white right-[15%] top-[50%] translate-y-[-50%] aspect-video h-[300px] shadow-lg"></div>
 			</li>
 			<li
-				class="border-b border-grey py-[1rem] flex gap-[1rem] items-center">
+				class="group border-b relative border-grey hover:border-black py-[1rem] flex gap-[1rem] items-center">
 				<div class="h-[70px] aspect-square border rounded-full"></div>
 				<p>Designer 2</p>
+				<div
+					class="pointer-events-none absolute hidden group-hover:block bg-white right-[20%] top-[50%] translate-y-[-50%] aspect-video h-[300px] shadow-lg"></div>
 			</li>
 			<li
-				class="border-b border-grey py-[1rem] flex gap-[1rem] items-center">
+				class="group border-b relative border-grey hover:border-black py-[1rem] flex gap-[1rem] items-center">
 				<div class="h-[70px] aspect-square border rounded-full"></div>
 				<p>Designer 3</p>
+				<div
+					class="pointer-events-none absolute hidden group-hover:block bg-white right-[15%] top-[50%] translate-y-[-50%] aspect-video h-[300px] shadow-lg"></div>
 			</li>
 			<li
-				class="border-b border-grey py-[1rem] flex gap-[1rem] items-center">
+				class="group border-b relative border-grey hover:border-black py-[1rem] flex gap-[1rem] items-center">
 				<div class="h-[70px] aspect-square border rounded-full"></div>
 				<p>Designer 4</p>
+				<div
+					class="pointer-events-none absolute hidden group-hover:block bg-white right-[20%] top-[50%] translate-y-[-50%] aspect-video h-[300px] shadow-lg"></div>
 			</li>
 		</ul>
 	</section>
