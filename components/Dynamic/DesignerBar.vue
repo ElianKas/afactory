55ff6b9d2373cb02f13a15c0a3199e65e3f8008c
ElianKas <135838274+ElianKas@users.noreply.github.com>
2025-01-26 17:04:57 +0100

    sb products embed

---
+++ components/Dynamic/DesignerBar.vue
@@ -0,0 +1,8 @@
+<script setup>
+	const props = defineProps({ name: String });
+</script>
+<template>
+	<nav>
+		<h1 class="text-2xl">{{ name }}</h1>
+	</nav>
+</template>
--- storyblok/Shop.vue
+++ storyblok/Shop.vue
@@ -1,19 +1,30 @@
 <script setup>
-	const { slug } = useRoute().params;
 	const props = defineProps({ blok: Object });
 	const storyblokApi = useStoryblokApi();
-	const { data } = await storyblokApi.get('cdn/stories', {
+	const { slug } = useRoute().params;
+	const { data: products } = await storyblokApi.get('cdn/stories', {
 		is_startpage: false,
 		content_type: 'product',
 		starts_with:
 			slug[0] /* receive products published by the designer (slug = designer name) */,
 	});
+	const { data: profil } = await storyblokApi.get('cdn/stories', {
+		is_startpage: false,
+		content_type: 'profil',
+		starts_with:
+			slug[0] /* receive products published by the designer (slug = designer name) */,
+	});
 	onMounted(() => {
-		console.log(data);
+		console.log(products);
+		console.log(profil);
 	});
 </script>
 <template>
 	<article>
-		{{ blok }}
+		<DynamicDesignerBar />
+		<h2>Bio</h2>
+		{{ blok.text }}
+		<h2>Produkte</h2>
+		<p>{{ products }}</p>
 	</article>
 </template>
