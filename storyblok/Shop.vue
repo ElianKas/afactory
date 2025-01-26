fb45082f691902d697f19933b8a7bf752591c4a1
ElianKas <135838274+ElianKas@users.noreply.github.com>
2025-01-26 16:31:37 +0100

    Update Shop.vue

--- storyblok/Shop.vue
+++ storyblok/Shop.vue
@@ -5,7 +5,8 @@
 	const { data } = await storyblokApi.get('cdn/stories', {
 		is_startpage: false,
 		content_type: 'product',
-		starts_with: slug.join('/'),
+		starts_with:
+			slug[0] /* receive products published by the designer (slug = designer name) */,
 	});
 	onMounted(() => {
 		console.log(data);
