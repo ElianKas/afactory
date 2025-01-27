b7ea7735ae98b8c7f7de745b539835c2c7fa9441
ElianKas <135838274+ElianKas@users.noreply.github.com>
2025-01-27 15:26:09 +0100

    Update Shop.vue

--- storyblok/Shop.vue
+++ storyblok/Shop.vue
@@ -8,20 +8,23 @@
 		starts_with:
 			slug[0] /* receive products published by the designer (slug = designer name) */,
 	});
-
 	function handleEmitProfile(profile) {
 		console.log(profile);
+		/* 
+		TODO
+		
+		*/
 	}
 	onMounted(() => {});
 </script>
 <template>
 	<article>
 		<DynamicDesignerBar @emit-profile="handleEmitProfile" />
-		<h2>Bio</h2>
-		<p>{{ blok.text }}</p>
-		<br />
-		<br />
 		<h2>Produkte</h2>
 		<p>{{ products }}</p>
+		<br />
+		<br />
+		<h2>Bio</h2>
+		<p>{{ blok.text }}</p>
 	</article>
 </template>
